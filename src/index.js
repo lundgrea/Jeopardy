bd231b9e58bd91e424f7222fead595c5d0d912bd
Jon O'Drobinak <jodrobinak@gmail.com>
2019-07-13 12:25:49 -0600

    Create intro, player info splash screens, temp game board, fade transitions

--- src/index.js
+++ src/index.js
@@ -18,10 +18,37 @@ import Player from './Player';
 import Round from './Round';
 import Turn from './Turn';
 
-$(document).ready(function() {
+function createPlayers(Player1, Player2, Player3) {
+  let player1 = new Player(Player1, 1)
+  let player2 = new Player(Player2, 2)
+  let player3 = new Player(Player3, 3)
+  return [player1, player2, player3]
+}
 
-  $('#player1-name__submit').click( () => {
- getPlayerName('#player1-name__input', '#player1-name__display')
+// $(window).load(function() {
+//   // will first fade out the loading animation
+// $('.welcome-message').delay(1500).fadeOut("slow");
+//   // will fade out the whole DIV that covers the website.
+// //$("#preloader").delay(2000).fadeOut("slow");
+// })
+$(document).ready(function() {
+  $('.user-name-inputs').hide();
+  $('.puzzle-table').hide();
+  $('.welcome-message').delay(2500).fadeOut("slow");
+  $('.user-name-inputs').append(`
+  <input type="text"id="player1-name__input"> Player 1 </input>
+  <p></p>
+  <input type="text"id="player2-name__input"> Player 2 </input>
+  <p></p>
+  <input type="text"id="player3-name__input" label="Player3">Player 3:</input>
+  <p></p>
+  <button id="player3-name__submit">Game on!</button>`).delay(3000).fadeIn('slow');
+  //$('.user-name-inputs').delay(3000).fadeIn("slow");
+
+  $('#player3-name__submit').click( () => {
+ getPlayerName('#player3-name__input', '#player3-name__display');
+ $('.user-name-inputs').fadeOut();
+ $('.puzzle-table').delay(1000).fadeIn();
 })
 
   function getPlayerName(playerInput, playerDisplay) {
