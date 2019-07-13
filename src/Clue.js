ff195360fa10ba551826c407e399254f76978404
Chris Lane <pankokrumbs@gmail.com>
2019-07-13 15:03:23 -0600

    Add a functional getCategory method to the class Clue

--- src/Clue.js
+++ src/Clue.js
@@ -1,6 +1,24 @@
+
+
 class Clue {
   constructor() {
+    this.usedCategories = []
+  }
+  generateRandomId(min, max) {
+    return Math.floor(Math.random() * max) + min
+  }
 
+  getCategory(dataObj, id) {
+    let dataArray = Object.keys(dataObj).reduce((acc, category) => {
+      if (this.usedCategories.includes(category) !== true) {
+        acc.push(category)
+      }
+      return acc
+    }, [])
+    let category = dataArray.splice(id, 1)[0]
+    this.usedCategories.push(category)
+    return category
+    
   }
 }
 
--- test/Clue-test.js
+++ test/Clue-test.js
@@ -2,11 +2,12 @@ import chai from 'chai';
 const expect = chai.expect;
 
 import Clue from '../src/Clue';
+import data from '../src/data';
 
-//Declare variables for beforEach here//
+var clue
 
 beforeEach(() => {
-
+  clue = new Clue()
 });
 
 describe('Clue', function() {
@@ -16,6 +17,27 @@ describe('Clue', function() {
     expect(Clue).to.be.a('function');
   });
 
-  it('should be able to pick a category')
+  it('should be able to generate a random number between a min and max. The number should be a whole number', function() {
 
+    let randomId = clue.generateRandomId(0, 9)
+    
+    expect(randomId >= 1).to.equal(true);
+    expect(randomId <= 9).to.equal(true);
+    expect(Number.isInteger(randomId)).to.equal(true);
+  });
+
+  it('should be able to find a category based of the random number generated', function() {
+
+    let category1 = clue.getCategory(data.categories, 0);
+    let category2 = clue.getCategory(data.categories, 3);
+    let category3 = clue.getCategory(data.categories, 7);
+
+    expect(category1).to.equal('unitedStatesHistory');
+    expect(category2).to.equal('nameThatBoardGame');
+    expect(category3).to.equal('cableTV');
+    expect(clue.usedCategories.length).to.equal(3)
+
+  })
+
+  // it('should be able to generate 3 categories from')
 });
