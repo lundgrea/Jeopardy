6bc446053fa25f371ef8216ae2189305867b8d93
Alyssa Lundgren <lundgrea@gmail.com>
2019-07-13 14:32:40 -0600

    Implement startGame(), generateRound() and generatePlayers() method on the Game class along with tests

--- src/Game.js
+++ src/Game.js
@@ -4,23 +4,21 @@ import Round from '../src/Round';
 
 class Game {
   constructor() {
-    this.currentRound;
+    this.currentRound = {};
     this.roundTracker = 0;
     this.players = []
-    
   }
 
   generateRound() {
-    this.currentRound = new Round()
-    console.log(this.currentRound)
+    this.currentRound = new Round();
   }
 
   generatePlayers(player1Name, player2Name, player3Name) {
     this.player1 = new Player(player1Name);
-    this.player2 = new Player(player2Name);
-    this.player3 = new Player(player3Name);
     this.players.push(this.player1);
+    this.player2 = new Player(player2Name);
     this.players.push(this.player2);
+    this.player3 = new Player(player3Name);
     this.players.push(this.player3);
     return this.players
   }
--- test/Game-test.js
+++ test/Game-test.js
@@ -16,6 +16,10 @@ describe('Game', function() {
     expect(Game).to.be.a('function');
   });
 
+  it('should hold a place for the current round', function(){
+    expect(game.currentRound).to.be.a('object');
+  });
+
   it('should start with current round as 0', function(){
     expect(game.roundTracker).to.equal(0);
   });
@@ -24,19 +28,22 @@ describe('Game', function() {
     expect(game.players).to.be.a('array');
   });
 
+it('should create be able to create new rounds', function(){
+    game.generateRound()
+    expect(game.currentRound).to.be.a('object');
+  }); 
+
   it('should create three new Players from the player input fields', function(){
     game.generatePlayers('Jon', 'Chris', 'Alyssa');
     expect(game.players).to.deep.equal([{name: 'Jon', score: 0} , {name: 'Chris', score: 0 } , { name: 'Alyssa', score: 0 }]);
   });
 
-
-  // it('should be able to start the game', function(){
-  //   expect(game.startGame()).to.be.a('function');
-  // });
-  
-  // it('should create new rounds', function(){
-  //   expect(game.generateRound()).to.equal(1);
-  // }); 
+  it('should be able to start the game', function(){
+    game.startGame();
+    expect(game.currentRound).to.be.a('object');
+    expect(game.roundTracker).to.equal(1);
+    expect(game.players).to.be.a('array')
+  });
 
   it('should keep track of the current round', function(){
     game.startGame()
