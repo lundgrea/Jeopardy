3ae03f3fb192633628060c48b09cdfaf43d9a1a5
Chris Lane <pankokrumbs@gmail.com>
2019-07-14 09:22:43 -0600

    Add a functional return boardColumnObj

--- src/Clue.js
+++ src/Clue.js
@@ -32,8 +32,16 @@ class Clue {
     let clues = dataArray.filter(clue => {
       return clue.categoryId === this.id
     })
+    return [this.getQuestion(clues, 100), this.getQuestion(clues, 200), this.getQuestion(clues, 300), this.getQuestion(clues, 400)]
   ///filter and include in array based off of point values///
   }
+
+  getQuestion(cluesArray, value) {
+    let cluePointArray = cluesArray.filter(clue => clue.pointValue === value)
+    let index = this.generateRandomId(0, cluePointArray.length)
+    return cluePointArray.splice(index, 1)
+  }
+
   createBoardColumnObj() {
     let obj = {
       category: this.getCategory(this.data.categories),
