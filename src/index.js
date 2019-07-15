f0e5d37bf64e1227d8166b1605f937782387081d
Jon O'Drobinak <jodrobinak@gmail.com>
2019-07-15 09:07:08 -0600

    Add packery elements, update landing page

--- src/index.js
+++ src/index.js
@@ -18,10 +18,19 @@ import Player from './Player';
 import Round from './Round';
 import Turn from './Turn';
 
-function createPlayers(Player1, Player2, Player3) {
-  let player1 = new Player(Player1)
-  let player2 = new Player(Player2)
-  let player3 = new Player(Player3)
+function createPlayers(playerInput) {
+  let player1 = new Player(playerInput[0], 100)
+  let player2 = new Player(playerInput[1], 200)
+  let player3 = new Player(playerInput[2], 300)
+  console.log(`Player1 name: ${player1.name}`);
+  console.log(`Player2 name: ${player2.name}`);
+  console.log(`Player3 name: ${player3.name}`);
+  $('#js-player1-name').text(player1.name || 'Player 1');
+  $('#player-1-score').text(player1.score || 0);
+  $('#js-player2-name').text(player2.name || 'Player 2');
+  $('#player-2-score').text(player2.score || 0);
+  $('#js-player3-name').text(player3.name || 'Player 3');
+  $('#player-3-score').text(player3.score || 0);
   return [player1, player2, player3]
 }
 
@@ -32,6 +41,7 @@ function createPlayers(Player1, Player2, Player3) {
 // //$("#preloader").delay(2000).fadeOut("slow");
 // })
 $(document).ready(function() {
+  $('#main-score-cards').hide();
   $('#user-name-inputs').hide();
   $('#puzzle-table').hide();
   $('#welcome-message').delay(2500).fadeOut("slow");
@@ -40,20 +50,28 @@ $(document).ready(function() {
   <p></p>
   <input type="text"id="player2-name__input"> Player 2 </input>
   <p></p>
-  <input type="text"id="player3-name__input" label="Player3">Player 3:</input>
+  <input type="text"id="player3-name__input"> Player 3:</input>
   <p></p>
   <button id="player3-name__submit">Game on!</button>`).delay(3000).fadeIn('slow');
   //$('.user-name-inputs').delay(3000).fadeIn("slow");
+  //let $grid = $('.grid').packery({itemSelector: '.grid-item', columnWidth: 100});
+  //$('.grid').packery({itemSelector: '.grid-item', gutter: 15, percentPosition: true, columnWidth: 100, });
+
 
   $('#player3-name__submit').click( () => {
- getPlayerName('#player3-name__input', '#player3-name__display');
+    console.log($('#player1-name__input').val());
+    let playerNames = [$('#player1-name__input').val(), $('#player2-name__input').val(),$('#player3-name__input').val()]
+ createPlayers(playerNames);
  $('#user-name-inputs').fadeOut();
+ $('#main-score-cards').delay(1000).fadeIn();
  $('#puzzle-table').delay(1000).fadeIn();
 })
 
-  function getPlayerName(playerInput, playerDisplay) {
+  function getPlayerName(playerInput) {
     console.log('Hey :');
-    let playerName = $(`${playerInput}`).val();
-    $(`${playerDisplay}`).text(playerName)
+    console.log(playerInput)
+    let playerName1 = playerInput[0];
+    console.log(playerName1);
+    //$(`${playerDisplay}`).text(playerName)
   }
 })
